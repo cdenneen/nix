5e23e420f110771f596303c6aefeaca8e0cbe9da
Chris Denneen <cdenneen@gmail.com>
2025-01-04 14:16:27 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description: 'Chris Denneen system config';
+
+  inputs = {
+    # Pin our primary nixpkgs repository. This is the main nixpkgs repository
+    # we'll use for our configurations. Be very careful changing this because
+    # it'll impact your entire system.
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
+    # Build a custom WSL installer
+    nixos-wsl.url = "github:nix-community/NixOS-WSL";
+    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  };
+
+  outputs = { self, nixpkgs, home-manager, sops-nix, darwin, ... }@inputs: let
+    # Overlays is the list of overlays we want to apply from flake inputs.
+    overlays = [
+
+    ];
+    
+    mkSystem = import ./lib/mksystem.nix {
+      inherit self overlays nixpkgs inputs;
+    };
+  in {
+
+    nixosConfigurations.vm-aarch64-utm = mkSystem "vm-aarch64-utm" rec {
+      system = "aarch64-linux";
+      user = "cdenneen";
+    };
+
+    darwinConfigurations.macbook = mkSystem "macbook-pro-m1" rec {
+      system = "aarch64-darwin";
+      user = "cdenneen";
+      darwin = true;
+    };
+  };
+}
---
+++ users/cdenneen/home-manager.nix
@@ -0,0 +1,289 @@
+{ isWSL, inputs, self, ... }:
+
+{ config, lib, pkgs, ... }:
+
+let
+  isDarwin = pkgs.stdenv.isDarwin;
+  isLinux = pkgs.stdenv.isLinux;
+
+  # For our MANPAGER env var
+  # https://github.com/sharkdp/bat/issues/1145
+  manpager = (pkgs.writeShellScriptBin "manpager" (if isDarwin then ''
+    sh -c 'col -bx | bat -l man -p'
+    '' else ''
+    cat "$1" | col -bx | bat --language man --style plain
+  ''));
+
+  homeDirectory = config.home.homeDirectory;
+in {
+  # Home-manager 22.11 requires this be set. We never set it so we have
+  # to use the old state version.
+  home.stateVersion = "18.09";
+
+  xdg.enable = true;
+
+  #---------------------------------------------------------------------
+  # Packages
+  #---------------------------------------------------------------------
+
+  # Packages I always want installed. Most packages I install using
+  # per-project flakes sourced with direnv and nix-shell, so this is
+  # not a huge list.
+  home.packages = [
+    pkgs._1password-cli
+    pkgs.asciinema
+    pkgs.awscli2
+    pkgs.atuin
+    pkgs.autojump
+    pkgs.bat
+    pkgs.direnv
+    pkgs.eza
+    pkgs.fd
+    pkgs.fzf
+    pkgs.gh
+    pkgs.gnupg
+    pkgs.htop
+    pkgs.jq
+    pkgs.k9s
+    pkgs.kubectl
+    pkgs.kubernetes-helm
+    pkgs.kubeswitch
+    pkgs.lazygit
+    pkgs.mysql84
+    pkgs.neovim
+    pkgs.ripgrep
+    pkgs.sentry-cli
+    pkgs.shellcheck
+    pkgs.terraform
+    pkgs.terragrunt
+    pkgs.tmux
+    pkgs.tree
+    pkgs.watch
+    pkgs.zoxide
+
+    # Node is required for Copilot.vim
+    pkgs.nodejs
+  ] ++ (lib.optionals isDarwin [
+    # This is automatically setup on Linux
+    pkgs.cachix
+    pkgs.tailscale
+  ]) ++ (lib.optionals (isLinux && !isWSL) [
+    pkgs.chromium
+    pkgs.firefox
+    pkgs.rofi
+    pkgs.valgrind
+    pkgs.zathura
+    pkgs.xfce.xfce4-terminal
+  ]);
+
+  #---------------------------------------------------------------------
+  # Env vars and dotfiles
+  #---------------------------------------------------------------------
+
+  home.sessionVariables = {
+    LANG = "en_US.UTF-8";
+    LC_CTYPE = "en_US.UTF-8";
+    LC_ALL = "en_US.UTF-8";
+    EDITOR = "nvim";
+    PAGER = "less -FirSwX";
+    MANPAGER = "${manpager}/bin/manpager";
+    DIRENV_WARN_TIMEOUT = "100s";
+  };
+
+  home.file = {
+    ".gdbinit".source = ./gdbinit;
+    ".inputrc".source = ./inputrc;
+    ".kube/switch-config.yaml".source = ./switch-config.yaml;
+  } // (if isDarwin then {
+    "Library/Application Support/jj/config.toml".source = ./jujutsu.toml;
+  } else {});
+
+  xdg.configFile = {
+    "zsh/07-functions.zsh".text = builtins.readFile ./zsh/07-functions.zsh;
+    "i3/config".text = builtins.readFile ./i3;
+    "rofi/config.rasi".text = builtins.readFile ./rofi;
+
+    "nvim".source = builtins.fetchGit {
+      url = "https://github.com/cdenneen/nvim";
+      rev = "fdbc03e040eb796fe1c51c2292015f2e5605d1b8";
+    };
+  } // (if isDarwin then {
+    # Rectangle.app. This has to be imported manually using the app.
+    "rectangle/RectangleConfig.json".text = builtins.readFile ./RectangleConfig.json;
+  } else {}) // (if isLinux then {
+    "ghostty/config".text = builtins.readFile ./ghostty.linux;
+    "jj/config.toml".source = ./jujutsu.toml;
+  } else {});
+
+  #---------------------------------------------------------------------
+  # Programs
+  #---------------------------------------------------------------------
+
+  programs.gpg.enable = true;
+
+  programs.bash = {
+    enable = true;
+    shellOptions = [];
+    historyControl = [ "ignoredups" "ignorespace" ];
+    initExtra = builtins.readFile ./bashrc;
+
+    shellAliases = {
+      ga = "git add";
+      gc = "git commit";
+      gco = "git checkout";
+      gcp = "git cherry-pick";
+      gdiff = "git diff";
+      gl = "git prettylog";
+      gp = "git push";
+      gs = "git status";
+      gt = "git tag";
+    };
+  };
+
+  programs.direnv= {
+    enable = true;
+
+    config = {
+      whitelist = {
+        prefix= [
+        ];
+
+        exact = ["$HOME/.envrc"];
+      };
+    };
+  };
+  
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+    userName = "Chris Denneen";
+    userEmail = "cdenneen@gmail.com";
+    ignores = [ ".DS_Store" "Thumbs.db" ];
+    signing = {
+      key = "BFEB75D960DFAA6B";
+      signByDefault = false;
+    };
+    aliases = {
+      cleanup = "!git branch --merged | grep  -v '\\*\\|master\\|develop' | xargs -n 1 -r git branch -d";
+      prettylog = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(r) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+      root = "rev-parse --show-toplevel";
+    };
+    extraConfig = {
+      branch.autosetuprebase = "always";
+      color.ui = true;
+      core.askPass = ""; # needs to be empty to use terminal for ask pass
+      credential.helper = "store"; # want to make this more secure
+      github.user = "cdenneen";
+      pull.rebase = "true";
+      push.default = "tracking";
+      push.autoSetupRemote = true;
+      init.defaultBranch = "main";
+    };
+  };
+
+  programs.jujutsu = {
+    enable = true;
+
+    # I don't use "settings" because the path is wrong on macOS at
+    # the time of writing this.
+  };
+
+  programs.tmux = {
+    enable = true;
+    terminal = "xterm-256color";
+    shortcut = "l";
+    secureSocket = false;
+    mouse = true;
+
+    extraConfig = ''
+      set -ga terminal-overrides ",*256col*:Tc"
+
+      set -g @dracula-show-battery false
+      set -g @dracula-show-network false
+      set -g @dracula-show-weather false
+
+      bind -n C-k send-keys "clear"\; send-keys "Enter"
+
+      run-shell ${sources.tmux-pain-control}/pain_control.tmux
+      run-shell ${sources.tmux-dracula}/dracula.tmux
+    '';
+  };
+
+  programs.alacritty = {
+    enable = !isWSL;
+
+    settings = {
+      env.TERM = "xterm-256color";
+
+      key_bindings = [
+        { key = "K"; mods = "Command"; chars = "ClearHistory"; }
+        { key = "V"; mods = "Command"; action = "Paste"; }
+        { key = "C"; mods = "Command"; action = "Copy"; }
+        { key = "Key0"; mods = "Command"; action = "ResetFontSize"; }
+        { key = "Equals"; mods = "Command"; action = "IncreaseFontSize"; }
+        { key = "Subtract"; mods = "Command"; action = "DecreaseFontSize"; }
+      ];
+    };
+  };
+
+  programs.kitty = {
+    enable = !isWSL;
+    extraConfig = builtins.readFile ./kitty;
+  };
+
+  programs.i3status = {
+    enable = isLinux && !isWSL;
+
+    general = {
+      colors = true;
+      color_good = "#8C9440";
+      color_bad = "#A54242";
+      color_degraded = "#DE935F";
+    };
+
+    modules = {
+      ipv6.enable = false;
+      "wireless _first_".enable = false;
+      "battery all".enable = false;
+    };
+  };
+
+  services.gpg-agent = {
+    enable = isLinux;
+    enableSshSupport = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    pinentryPackage = pkgs.pinentry-tty;
+
+    # cache the keys forever so we don't get asked for a password
+    defaultCacheTtl = 31536000;
+    maxCacheTtl = 31536000;
+  };
+
+  xresources.extraConfig = builtins.readFile ./Xresources;
+
+  # Make cursor not tiny on HiDPI screens
+  home.pointerCursor = lib.mkIf (isLinux && !isWSL) {
+    name = "Vanilla-DMZ";
+    package = pkgs.vanilla-dmz;
+    size = 128;
+    x11.enable = true;
+  };
+
+  sops = {
+    age.keyFile = "${homeDirectory}/.config/sops/age/keys.txt";
+    defaultSopsFile = "${self}/users/cdenneen/secrets/secret.yaml";
+    validateSopsFiles = false;
+  };
+
+  sops.secrets = {
+    "gpg_gmail" = {
+      path = "${homeDirectory}/.gnupg/private-keys-v1.d/personal.key";
+      mode = "0400";
+    };
+    "gpg_ap" = {
+      path = "${homeDirectory}/.gnupg/private-keys-v1.d/work.key";
+      mode = "0400";
+    };
+  };
+}
