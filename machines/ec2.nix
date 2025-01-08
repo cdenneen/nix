1e37db793198da6222059b2e30dde32a4a444983
Chris Denneen <cdenneen@gmail.com>
2025-01-08 14:16:28 -0500

    PermitRootLogin amazon-image default, mkForce

--- machines/ec2.nix
+++ machines/ec2.nix
@@ -6,7 +6,7 @@
   ec2.efi = true;
 
   services.udisks2.enable = lib.mkForce false;
-  services.openssh.settings.PermitRootLogin = "prohibit-password";
+  services.openssh.settings.PermitRootLogin = lib.mkForce "prohibit-password";
 
   # Lots of stuff that uses aarch64 that claims doesn't work, but actually works.
   nixpkgs.config.allowUnfree = true;
