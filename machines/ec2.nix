de3d1d2a58893d800067e10972f4991e2e6a084b
Chris Denneen <cdenneen@gmail.com>
2025-01-08 14:13:26 -0500

    force udisk2 to false

--- machines/ec2.nix
+++ machines/ec2.nix
@@ -5,6 +5,8 @@
   ];
   ec2.efi = true;
 
+  services.udisks2.enable = lib.mkforce false;
+
   # Lots of stuff that uses aarch64 that claims doesn't work, but actually works.
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.allowUnsupportedSystem = true;
