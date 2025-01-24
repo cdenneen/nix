95df1edf42b47aa0dc643994eb5a4d3d3fb33684
Chris Denneen <cdenneen@gmail.com>
2025-01-24 11:06:33 -0500

    Update format,repo,rev,hash in overlay

--- overlays/oci-cli.nix
+++ overlays/oci-cli.nix
@@ -1,9 +1,15 @@
+let
+  version = "3.51.4";
+in
 self: super: {
   oci-cli = super.oci-cli.overrideAttrs (old: {
-    version = "3.51.4";
+    version = version;
+    format = "setuptools";
     src = super.fetchFromGitHub {
       owner = "oracle";
-      hash = "sha256-lqqSx4jxQVq2pjVv9lvaX6nNK6OqtMjPqOtLMLpVMUU=";
+      repo = "oci-cli";
+      rev = "v${version}";
+      hash = "sha256-yooEZuSIw2EMJVyT/Z/x4hJi8a1F674CtsMMGkMAYLg=";
     };
   });
 }
